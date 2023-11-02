eeded25f8a1657432ee7ca713136f75276688122
Debadatta <debadatta30@gmail.com>
2023-11-02 11:24:13 +0000

    Added ec2 start and stop

---
+++ python/stop.py
@@ -0,0 +1,31 @@
+import boto3
+import logging
+ec2 = boto3.resource('ec2')
+
+ec2c = boto3.client('ec2')
+
+# Set up the logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    # create filter for instances in running state
+    filters = [
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['running']
+        }
+    ]
+    
+    # filter the instances based on filters() above
+    instances = ec2.instances.filter(Filters=filters)
+
+    # instantiate empty array
+    RunningInstances = []
+
+    for instance in instances:
+        # for each instance, append to array and print instance id
+        RunningInstances.append(instance.id)
+        print (instance.id)
+        
+    ec2c.stop_instances(InstanceIds=RunningInstances)
